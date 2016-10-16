a326b6a79842a5950bae71ea945a192ca437c086
niels <niels@DESKTOP-CE6ILBP>
2016-10-16 20:27:33 +0200

    counter added

--- src/Combinatoriek_Practical_Assignment/Derangements.java
+++ src/Combinatoriek_Practical_Assignment/Derangements.java
@@ -14,55 +14,54 @@ import java.util.ArrayList;
  */
 public class Derangements {
 
-    private ArrayList<Integer> oplossing;  //ArrayList met de oplossing.
-    private ArrayList<Integer> ongebruikt; // ArrayList met de ongebruikte integers.
-    private int oplossingen;
+    private ArrayList<Integer> solutions;  //ArrayList met de oplossing.
+    private ArrayList<Integer> unused; // ArrayList met de ongebruikte integers.
+    private int counter;
 
     public Derangements(int n) {
-        this.oplossing = fillOplossing(n);
-        this.ongebruikt = fillOngebruikt(n);
-        this.oplossingen = 0;
+        this.solutions = fillSolutions(n);
+        this.unused = fillUnused(n);
+        this.counter = 0;
     }
 
     public void start(int plaatsNr) {
         recursion(plaatsNr);
-        System.out.println(oplossingen + " derangements.");
     }
 
     public void visualize() {
-        for (int i = 0; i < 14; i++) {
+        for (int i = 0; i < 23; i++) {
             System.out.println(i + ": " + countDerangements(i) + " derangements");
         }
     }
 
     public void recursion(int plaatsNr) {
-        if (plaatsNr >= oplossing.size()) {
-            print(oplossing);
-            oplossingen++;
+        if (plaatsNr >= solutions.size()) {
+            print(solutions);
+            counter++;
         }
-        for (int i = 0; i < oplossing.size(); i++) {
-            if (ongebruikt.get(i) != plaatsNr + 1 && ongebruikt.get(i) != 0) {
-                oplossing.set(plaatsNr, ongebruikt.get(i));
-                ongebruikt.set(i, 0);
+        for (int i = 0; i < solutions.size(); i++) {
+            if (unused.get(i) != plaatsNr + 1 && unused.get(i) != 0) {
+                solutions.set(plaatsNr, unused.get(i));
+                unused.set(i, 0);
                 recursion(plaatsNr + 1);
-                ongebruikt.set(i, oplossing.get(plaatsNr));
-                oplossing.set(plaatsNr, 0);  //
+                unused.set(i, solutions.get(plaatsNr));
+                solutions.set(plaatsNr, 0);  //
             }
         }
     }
 
-    private int countDerangements(int n) {
+    public long countDerangements(int n) {
         if (n == 0) {
-            return 1;
+            return 1L;
         }
         if (n == 1) {
-            return 0;
+            return 0L;
         } else {
             return (n - 1) * (countDerangements(n - 1) + countDerangements(n - 2));
         }
     }
 
-    private ArrayList<Integer> fillOplossing(int n) {
+    private ArrayList<Integer> fillSolutions(int n) {
         ArrayList<Integer> array = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             array.add(0);
@@ -71,7 +70,7 @@ public class Derangements {
         return array;
     }
 
-    private ArrayList<Integer> fillOngebruikt(int n) {
+    private ArrayList<Integer> fillUnused(int n) {
         ArrayList<Integer> array = new ArrayList<Integer>();
         for (int i = 0; i < n; i++) {
             array.add(i + 1);
@@ -101,5 +100,9 @@ public class Derangements {
         sb.setLength(sb.length() - 1);
         sb.append("}");
         System.out.println(sb);
+    }
+    
+    public int getCounter(){
+        return counter;
     }
 }
--- src/Combinatoriek_Practical_Assignment/Main.java
+++ src/Combinatoriek_Practical_Assignment/Main.java
@@ -1,19 +1,17 @@
-
 package Combinatoriek_Practical_Assignment;
 
 import java.util.Scanner;
 
 /**
- * @author Medard van Leeuwe    s4530608
- * @author Nick Nibbeling       s4616146
- * @author Niels van Drueten    s4496604
- * 
+ * @author Medard van Leeuwe s4530608
+ * @author Nick Nibbeling s4616146
+ * @author Niels van Drueten s4496604
+ *
  * @version 1.1
  * @date 16 october 2016
  */
 public class Main {
 
-    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Given a positive integer n, visualize all the derangements of the set {1,2,3,...,n}.");
@@ -21,20 +19,26 @@ public class Main {
         System.out.println("to visualize all these derangements.");
         System.out.print("n: ");
         int n = scanner.nextInt();
-        Derangements d = new Derangements(n);
-        d.start(0);
-        
-        System.out.print("visualize derangements? [y/n]: ");
-        char c = scanner.next().charAt(0);
-        switch (c){
-            case 'y':
-                d.visualize();
-                break;
-            case 'n':
-                break;
-            default:
-                System.out.println("Wrong answer");
+        if (n < 0) {
+            System.out.println("n cannot be negative.");
+        } else if (n == 0) {
+            System.out.println("{}");
+            System.out.println("1 derangement.");
+        } else {
+            Derangements d = new Derangements(n);
+            d.start(0);
+            System.out.println(d.getCounter() + " derangements.");
+            System.out.print("visualize derangements? [y/n]: ");
+            char c = scanner.next().charAt(0);
+            switch (c) {
+                case 'y':
+                    d.visualize();
+                    break;
+                case 'n':
+                    break;
+                default:
+                    System.out.println("Wrong answer");
+            }
         }
     }
-    
 }
