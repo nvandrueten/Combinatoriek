694c1c8a711ee694ba92b794b2514836b2e1126d
niels <niels@DESKTOP-CE6ILBP>
2016-10-16 19:16:52 +0200

    counter added

--- src/Combinatoriek_Practical_Assignment/Derangements.java
+++ src/Combinatoriek_Practical_Assignment/Derangements.java
@@ -5,97 +5,97 @@ import java.util.ArrayList;
 
 /**
  *
- * @author Medard van Leeuwe    s4530608
- * @author Nick Nibbeling       s4616146
- * @author Niels van Drueten    s4496604
- * 
- * @version 1.0
- * @date 14 october 2016
+ * @author Medard van Leeuwe s4530608
+ * @author Nick Nibbeling s4616146
+ * @author Niels van Drueten s4496604
+ *
+ * @version 1.1
+ * @date 16 october 2016
  */
 public class Derangements {
 
     private ArrayList<Integer> oplossing;  //ArrayList met de oplossing.
     private ArrayList<Integer> ongebruikt; // ArrayList met de ongebruikte integers.
     private int oplossingen;
-    
+
     public Derangements(int n) {
         this.oplossing = fillOplossing(n);
         this.ongebruikt = fillOngebruikt(n);
         this.oplossingen = 0;
     }
 
-    public void start(int plaatsNr){
+    public void start(int plaatsNr) {
         recursion(plaatsNr);
-        System.out.println("Aantal oplossingen: " + oplossingen);
-        String s = "";
-        for (int i = 0; i < 14; i++){
+        System.out.println(oplossingen + " derangements.");
+    }
+
+    public void visualize() {
+        for (int i = 0; i < 14; i++) {
             System.out.println(i + ": " + countDerangements(i) + " derangements");
         }
     }
-    
-    
+
     public void recursion(int plaatsNr) {
-        if (plaatsNr >= oplossing.size()){
+        if (plaatsNr >= oplossing.size()) {
             print(oplossing);
             oplossingen++;
         }
         for (int i = 0; i < oplossing.size(); i++) {
-
-            if (ongebruikt.get(i) != plaatsNr + 1 && ongebruikt.get(i) != 0 ) {
+            if (ongebruikt.get(i) != plaatsNr + 1 && ongebruikt.get(i) != 0) {
                 oplossing.set(plaatsNr, ongebruikt.get(i));
                 ongebruikt.set(i, 0);
-                recursion(plaatsNr+1);
-                ongebruikt.set(i,oplossing.get(plaatsNr));
+                recursion(plaatsNr + 1);
+                ongebruikt.set(i, oplossing.get(plaatsNr));
                 oplossing.set(plaatsNr, 0);  //
             }
         }
     }
-    
-    private int countDerangements(int n){
-        if (n==0)
+
+    private int countDerangements(int n) {
+        if (n == 0) {
             return 1;
-        if (n==1)
+        }
+        if (n == 1) {
             return 0;
-        else {
-            return (n-1)*(countDerangements(n-1) + countDerangements(n-2));
+        } else {
+            return (n - 1) * (countDerangements(n - 1) + countDerangements(n - 2));
         }
     }
-    
+
     private ArrayList<Integer> fillOplossing(int n) {
         ArrayList<Integer> array = new ArrayList<>();
-        for (int i = 0; i < n ; i++){
+        for (int i = 0; i < n; i++) {
             array.add(0);
         }
-        
+
         return array;
     }
 
     private ArrayList<Integer> fillOngebruikt(int n) {
         ArrayList<Integer> array = new ArrayList<Integer>();
-        for (int i = 0  ; i < n ; i++){
-            array.add(i+1);
+        for (int i = 0; i < n; i++) {
+            array.add(i + 1);
         }
         return array;
     }
-    
-    private void print(ArrayList<Integer> oplossing){
+
+    private void print(ArrayList<Integer> oplossing) {
         String result = "{";
-        for ( int i = 0; i < oplossing.size(); i++){
-            
-            if (i == oplossing.size() - 1){
+        for (int i = 0; i < oplossing.size(); i++) {
+
+            if (i == oplossing.size() - 1) {
                 result += oplossing.get(i) + "}";
-            }
-            else {
-            result += oplossing.get(i) + ",";
+            } else {
+                result += oplossing.get(i) + ",";
             }
         }
         System.out.println(result);
     }
-    
-    private void printSB(ArrayList<Integer> oplossing){
+
+    private void printSB(ArrayList<Integer> oplossing) {
         StringBuilder sb = new StringBuilder();
         sb.append("{");
-        for (Integer i : oplossing){
+        for (Integer i : oplossing) {
             sb.append(i + ",");
         }
         sb.setLength(sb.length() - 1);
--- src/Combinatoriek_Practical_Assignment/Main.java
+++ src/Combinatoriek_Practical_Assignment/Main.java
@@ -8,18 +8,33 @@ import java.util.Scanner;
  * @author Nick Nibbeling       s4616146
  * @author Niels van Drueten    s4496604
  * 
- * @version 1.0
- * @date 14 october 2016
+ * @version 1.1
+ * @date 16 october 2016
  */
 public class Main {
 
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Given a positive integer n, visualize all the derangements of the set {1,2,3,...,n}.");
+        System.out.println("Perform some analysis that shows for which values of n it is no longer possible ");
+        System.out.println("to visualize all these derangements.");
         System.out.print("n: ");
         int n = scanner.nextInt();
         Derangements d = new Derangements(n);
         d.start(0);
+        
+        System.out.print("visualize derangements? [y/n]: ");
+        char c = scanner.next().charAt(0);
+        switch (c){
+            case 'y':
+                d.visualize();
+                break;
+            case 'n':
+                break;
+            default:
+                System.out.println("Wrong answer");
+        }
     }
     
 }
